a5de0832164de8df5a226ca5515bf7515b23892f
SergeyD <sergey.davydov@gs-labs.ru>
2023-08-24 16:32:13 +0300

    2nd

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = ">= 0.65.0"
+    }
+  } 
+}
+provider "yandex" { #Авторизация на провайдере
+  token     = "y0_AgAAAAAGCWnOAATuwQAAAADjSdxhqjrcJXB2Sn2IHpYxwiBNJG-eaUs"
+  cloud_id  = "b1g4ad9o2c4td783fgb0"
+  folder_id = "b1gb97ldb2ei7rbstmo7"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_vpc_network" "mynetwork-001" { #Создаю сеть
+  name = "mynetwork-001"
+}
+
+resource "yandex_vpc_subnet" "subnet" { #Подсеть
+  name           = "subnet01"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.mynetwork-001.id
+  v4_cidr_blocks = ["192.168.11.0/24"]
+}
+
+module "swarm_cluster" { #Подключаю модуль
+  source        = "./modules"
+  vpc_subnet_id = yandex_vpc_subnet.subnet.id
+  managers      = 1
+  workers       = 2
+}
